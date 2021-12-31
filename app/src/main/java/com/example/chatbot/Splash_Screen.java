d1bd0e1a374930bbae5c2178bee58373050925ea
Utkarsh Arora <arorautkarsh2002@gmail.com>
2021-12-31 12:53:46 +0530

    added splash screen

---
+++ app/src/main/java/com/example/chatbot/Splash_Screen.java
@@ -0,0 +1,38 @@
+package com.example.chatbot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class Splash_Screen extends AppCompatActivity {
+    TextView appname;
+    LottieAnimationView lottie;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        appname=findViewById(R.id.appname);
+        lottie=findViewById(R.id.lottie_splash_screen);
+
+        appname.animate().translationY(-1550).setDuration(2000).setStartDelay(0);
+        lottie.animate().translationX(2000).setDuration(250).setStartDelay(3700);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent i= new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+
+            }
+        },4000);
+    }
+}
